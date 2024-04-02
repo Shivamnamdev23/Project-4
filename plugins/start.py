9c923c58f6707926d1edf3960dcc4fdd2c2df1d8
Shivamnamdev23 <124493494+Shivamnamdev23@users.noreply.github.com>
2024-04-02 03:34:25 -0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -82,9 +82,8 @@ async def start_command(client: Client, message: Message):
                 pass
             
         k = await client.send_message(chat_id=message.from_user.id, text="<b>This video/file will be deleted in 10 minutes (Due to copyright issues).\n\n📌 Please forward this video/file to somewhere else and start downloading there.</b>")
-        await asyncio.sleep(10)
+        await asyncio.sleep(5)
         await m.delete()
-        await asyncio.sleep(10)
         await f.delete()
         await k.edit_text("Your video/file is successfully deleted!")
         return
