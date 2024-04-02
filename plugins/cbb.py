e69c5f3dd56cd45732028d163d719c49455223c1
Shivamnamdev23 <124493494+Shivamnamdev23@users.noreply.github.com>
2024-04-02 00:03:11 -0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -19,10 +19,10 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>OUR PUBLIC BOT :</b> <a href='https://t.me/publicfilesharerobot'> file share bot </a>  \n<b>📢 Channel :</b> <a href='https://t.me/Publicfille'>Public Channel</a> \n<b>🧑‍💻 Developer :</b> <a href='t.me/infoxe'> 𝙃𝙖𝙢𝙯𝙖 </a>",
+            text = f"<b>OUR PUBLIC BOT :</b> <a href='https://t.me/'> file share bot </a>  \n<b>📢 Channel :</b> <a href='https://t.me/'>Public Channel</a> \n<b>🧑‍💻 Developer :</b> <a href='t.me/'> 𝙃𝙖𝙢𝙯𝙖 </a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
-                [ [ InlineKeyboardButton(" Source code ", url="https://t.me/+NeqCUg-QDxo2Nzll") ],
+                [ [ InlineKeyboardButton(" Source code ", url="https://t.me/") ],
                     [
                         InlineKeyboardButton("🔒 Close", callback_data = "close")
                     ]
